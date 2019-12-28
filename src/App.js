8bf2d634520a54c6a5912a2254517e929b307022
andgmartins <and.gmartins@gmail.com>
2019-12-27 23:40:12 -0300

    Configuring Reactotron

--- src/App.js
+++ src/App.js
@@ -3,15 +3,13 @@ import { Router } from 'react-router-dom';
 import Routes from './routes';
 import GlobalStyle from './styles/global';
 import history from './services/history';
-
-// import { Container } from './styles';
+import './config/ReactotronConfig';
 
 export default function App() {
   return (
     <Router history={history}>
           <Routes />
           <GlobalStyle />
-          {/* <ToastContainer autoClose={3000} /> */}
     </Router>
   );
 }
--- src/routes/Route.js
+++ src/routes/Route.js
@@ -10,7 +10,7 @@ export default function RouteWrapper({
   isPrivate,
   ...rest
 }) {
-  const signed = true;
+  const signed = false;
 
   const Layout = signed ? DefaultLayout : AuthLayout;
 
